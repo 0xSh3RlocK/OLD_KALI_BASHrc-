20f3ca6b71fce976b0e8897462752f3e5059a82b
Ahmed Shams <96126679+0xSh3RlocK@users.noreply.github.com>
2022-03-01 13:38:23 -0800

    Add files via upload

---
+++ old_kali.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Overwritting .bashrc
+echo ===================
+sudo chsh -s /bin/bash $(whoami)
+wget https://raw.githubusercontent.com/ware255/kali-2019.2_bashrc/main/.bashrc -O ~/.bashrc
+echo ######################################################################
+echo                           REINICIANDO
+echo ######################################################################
+echo -n "3," && sleep 1 && echo -n "2," && sleep 1 && echo -n "1..." && sleep 1 && echo BOOOM && sudo reboot
