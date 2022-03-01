fd4449d8d2ed3d6ac9d76566175707e5d1a55451
Ahmed Shams <96126679+0xSh3RlocK@users.noreply.github.com>
2022-03-01 23:40:52 +0200

    Update old_kali.sh

--- old_kali.sh
+++ old_kali.sh
@@ -4,6 +4,6 @@ echo ===================
 sudo chsh -s /bin/bash $(whoami)
 wget https://raw.githubusercontent.com/ware255/kali-2019.2_bashrc/main/.bashrc -O ~/.bashrc
 echo ######################################################################
-echo                           REINICIANDO
+echo                            READY
 echo ######################################################################
 echo -n "3," && sleep 1 && echo -n "2," && sleep 1 && echo -n "1..." && sleep 1 && echo BOOOM && sudo reboot
